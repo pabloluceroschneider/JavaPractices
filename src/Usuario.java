b720d15d504b1312b32efb79db23f1a958c6ab85
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-09 23:32:23 -0300

    Add UsuarioCtrl

--- src/Usuario.java
+++ src/Usuario.java
@@ -6,11 +6,15 @@ public class Usuario {
     private String email;
     private String password;
 
-    public Usuario(){
-        this.name = "Pablo";
-        this.lastName = "Lucero";
-        this.email = "pabloluceroschneider@gmail.com";
-        this.password = "1234";
+    public Usuario( String name , String lastName, String email, String password){
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName(){
+        return this.name;
     }
 
     
---
+++ src/UsuarioCtrl.java
@@ -0,0 +1,14 @@
+package src;
+
+public class UsuarioCtrl {
+
+    public static void main(String[] args){
+
+        System.out.println("Mi primer programa hecho en Java");
+
+        Usuario usr1 = new Usuario("Pablo", "Lucero Schneider", "pdls@gmail.com","12345678");
+        System.out.println(usr1.getName());
+
+    }
+    
+}

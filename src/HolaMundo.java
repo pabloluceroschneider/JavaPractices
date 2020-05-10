f575351893391b18aa11040fa9d83a5223bf404a
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-09 23:07:04 -0300

    Clase Usuario

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,10 +1,16 @@
 package src;
 
-public class HolaMundo{
+import java.util.Scanner;
+
+public class HolaMundo {
 
     public static void main(String[] args){
 
         System.out.println("Mi primer programa hecho en Java");
+
+        Usuario usr1 = new Usuario();
+        System.out.println(usr1);
+
 
     }
 
---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Usuario {
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Usuario(){
+        this.name = "Pablo";
+        this.lastName = "Lucero";
+        this.email = "pabloluceroschneider@gmail.com";
+        this.password = "1234";
+    }
+
+    
+}
